431f5e229a2b2fbcc1025b672ef4e7a7817ea42f
Rafael Tognon <rafaelrovanitognon@gmail.com>
2022-11-02 00:43:45 -0300

    change port to 8000

--- app.js
+++ app.js
@@ -95,8 +95,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", apiRouter);
 
-app.listen(5000, () => {
-    console.log(`Example app listening on port 5000`);
+app.listen(8000, () => {
+    console.log(`Example app listening on port 8000`);
 });
 
 module.exports = app;
