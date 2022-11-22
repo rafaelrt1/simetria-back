5510e75548c257791678a2099a64ac7468bcdf34
Rafael Tognon <rafaelrovanitognon@gmail.com>
2022-11-22 10:09:51 -0300

    teste

--- gerencianet.js
+++ gerencianet.js
@@ -7,6 +7,8 @@ if (process.env.NODE_ENV !== "production") {
     require("dotenv").config();
 }
 
+console.log(process.env.GN_CERT);
+
 const cert = fs.readFileSync(
     path.resolve(__dirname, `./certs/${process.env.GN_CERT}`)
 );
