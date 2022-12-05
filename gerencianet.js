9f72cf05acf541dcf784bd4f5334ed99efd070f1
Rafael Tognon <rafaelrovanitognon@gmail.com>
2022-12-05 20:00:03 -0300

    Add console log on token generator

--- gerencianet.js
+++ gerencianet.js
@@ -17,7 +17,7 @@ const authenticate = ({ clientId, clientSecret }) => {
     const credentials = Buffer.from(`${clientId}:${clientSecret}`).toString(
         "base64"
     );
-
+    console.log(credentials, agent, process.env.GN_ENDPOINT);
     return axios({
         method: "POST",
         url: `${process.env.GN_ENDPOINT}/oauth/token`,
